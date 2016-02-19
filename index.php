358988d05ed561244d11af2369d7c5d4b5c4dc4d
EmilyThompson <Thompso_emil@bentley.edu>
2016-02-19 13:11:14 -0500

    Added files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+
+<head>
+<title>Find a Falcon</title>
+</head>
+
+<body>
+<?php
+echo '
+<html>
+<img src="C:\Users\Thompso_emil\Documents\Senior Year\CS 460\Find a falcon FINAL logo.png" align="middle" title="Logo of a company" alt="Logo of a company" />
+
+</html>
+';
+?>
+
+
+<table>
+		<tr>
+			<th colspan="3" valign="top"
+				style="padding-left: 5px; padding-top: 80px;"><p
+					style="width: 415px; word-wrap: normal; text-align: center; valign: top; font-family: Ebrima; font-size:20px">
+					Welcome to Find A Falcon! This application is designed for Bentley
+					University students and recent graduates who are looking for a
+					roommate. After filling out your profile preferences you will be
+					matched with a compatible roommate in no time! Check out our
+					testimonial video below!</p></th>
+			<th colspan="2" valign="top"><img
+				src="C:\Users\Thompso_emil\Documents\Senior Year\CS 460\Divider.png"></th>
+			<th colspan="3" style="padding: 50px" align="left">
+				<form style="width: 300px; font-family: Verdana">
+					<div style="height: 50px; padding-top: 10px">
+						<label for="Username"> Username (Bentley Email): <br> <input
+							size="50" maxlength="24" type="text" name="Username"> <br>
+						</label>
+					</div>
+					<div style="height: 50px">
+						<label for="Password"> Password: <br>
+						<input size="50" type="password" name="Password"><br>
+						</label>
+					</div>
+					<div style="height: 50px" align="center">
+						<label for="loginbutton">
+							<button id="submit">Login</button>
+						</label>
+					</div>
+					<div align="center">
+						<p>OR</p>
+					</div>
+					<div style="height: 50px;" align="center">
+						<label for="createaccountbutton">
+							<button id="submit">Create Account</button>
+						</label>
+					</div>
+				</form>
+			</th>
+		</tr>
+	</table>
+
+
+</body>
+
+<style type="text/css">
+#submit {
+	background-color: #4E79CB;
+	padding: .5em;
+	border-radius: 6px;
+	color: #fff;
+	font-family: 'Ebrima';
+	font-size: 20px;
+	text-decoration: bold;
+	border: none;
+}
+
+#submit:hover {
+	border: 2px solid black
+}
+</style>
+</html>

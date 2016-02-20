582363d0fe2af7d42c0c5b93436facc5a27c1c4a
EmilyThompson <Thompso_emil@bentley.edu>
2016-02-20 13:24:38 -0500

    Added files via upload

---
+++ homepage.php
@@ -0,0 +1,54 @@
+<html>
+
+<head>
+<title>Find a Falcon</title>
+</head>
+
+<body>
+	<div id="page-wrap" align="center"> <!-- CENTER WHOLE DOC -->
+
+		<img src="Find a Falcon FINAL logo.png" align="middle" title="Logo"
+			alt="LOGO" /> ';
+	<div id="navigation">
+<style>
+ul#menu {
+    padding: 0;
+}
+
+ul#menu li {
+    display: inline;
+}
+
+ul#menu li a {
+    color: black;
+    padding: 10px 20px;
+    text-decoration: none;
+    border-radius: 4px 4px 0 0;
+    font-family:Ebrima;
+    font-weight:bold;
+    font-size:20px;
+}
+
+ul#menu li a:hover {
+    text-decoration: underline;
+}
+</style>
+
+<ul id="menu">
+  <li><a href="/html/default.asp">HOME</a></li>
+  <li><a href="/css/default.asp">MY LIKES</a></li>
+  <li><a href="/js/default.asp">MY PROFILE PREFERENCES</a></li>
+  <li><a href="default.asp">ABOUT</a></li>
+  <li><a href="loginpage.php">LOGOUT</a></li>
+</ul> 
+	
+	
+
+
+</div>
+</div>
+
+
+
+</body>
+</html>
